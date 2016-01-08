95bbd6be0b4a9f9ac656778f57e066b54807b3ff
pyro001 <agnibiswasranchi@gmail.com>
2016-01-08 16:40:07 +0530

    Create initializer.lua

---
+++ initializer.lua
@@ -0,0 +1,17 @@
+ssid = "Name of wifi"
+pass = "password"
+
+-- Configure Wireless Internet
+print('\nAll About Circuits init.lua\n')
+wifi.setmode(wifi.STATION)
+print('set mode=STATION (mode='..wifi.getmode()..')\n')
+print('MAC Address: ',wifi.sta.getmac())
+print('Chip ID: ',node.chipid())
+print('Heap Size: ',node.heap(),'\n')
+-- wifi config start
+wifi.sta.config(ssid,pass)
+-- wifi config end
+
+-- Run the main file
+dofile("main.lua")
+
